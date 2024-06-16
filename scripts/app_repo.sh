d425e445a8d5341120b8e21d1573e1f5f1a5e21b
Itibere10 <itibere.filho@gmail.com>
2024-06-16 17:02:47 -0300

    Ajustando credenciais

--- scripts/app_repo.sh
+++ scripts/app_repo.sh
@@ -1,11 +1,12 @@
-USR='itibere10'
-TKN='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'
-
-if [ $# -ne 1 ]; then
-    echo "[ADA]: Utilize: $0 <name> "
+if [ $# -ne 3 ]; then
+    echo "[ADA]: Utilize: $0 <name> <user> <key>"
     exit 1
 fi
 
+NAME=$1
+USR=$2
+TKN=$3
+
 echo "[ADA]: Criando repositório Github para a aplicação $1..."
-curl -u $USR:$TKN -d '{"name":'$1',"private":false}' https://api.github.com/user/repos
+curl -u $USR:$TKN -d '{"name":'$NAME',"private":false}' https://api.github.com/user/repos
 echo "[ADA]: Script de criação do repositório remoto finalizado!"
