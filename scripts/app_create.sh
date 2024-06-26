958ab9e87d58ce17daf046fe49df765c8cc89898
Itibere10 <itibere.filho@gmail.com>
2024-06-25 23:15:20 -0300

    [DAA]: Criação inicial da aplicação pythoncripts

--- scripts/app_create.sh
+++ scripts/app_create.sh
@@ -1,34 +1,42 @@
 # Verifica se o número correto de argumentos foi passado
-if [ $# -ne 4 ]; then
-    echo "[DAA]: Utilize: $0 <type> <name> <public/private> <user/account>"
+
+if [ $# -ne 3 ]; then
+    echo "[DAA]: Utilize: $0 <type> <name> <local/github>"
     exit 1
 fi
 
 # Atribui os argumentos às variáveis correspondentes
 TYPE=$1
 NAME=$2
-STATUS=$3
-ACCOUNT=$4
-MODEL_PATH=""
+DESTINATION=$3
 
-# Criando um modelo de aplicação em Frontend
-if [ "$TYPE" = "frontend-simple" ]; then
-    echo "[DAA]: Realizando a criação da aplicação de frontend simples..."
-    MODEL_PATH="../models/frontend-simple/*"
-elif [ "$TYPE" = "frontend-react" ]; then
-    echo "[DAA]: Realizando a criação da aplicação de frontend com react..."
-    MODEL_PATH="../models/frontend-react/*"
+# Obtém o caminho do modelo
+MODEL_PATH="$(./app_get_types.sh $TYPE)"
+
+# Criação local
+if [ "$DESTINATION" = "local" ]; then
+    echo "[DAA]: Realizando a criação do repositório local para a aplicação $NAME..."
+    mkdir -p ../../$NAME
+    cp -r $MODEL_PATH ../../$NAME
+    cd ../../$NAME
+    echo "[DAA]: Repositório da aplicação $NAME criado com sucesso!"
+
+# Criação remota + local
+elif [ "$DESTINATION" = "github" ]; then
+    echo "[DAA]: Realizando a criação do repositório remoto (Github) para a aplicação $NAME..."
+    echo "[DAA]: O repositório também será clonado e sincronizado localmente..."
+    STATUS="$(./app_repo_configs status)"
+    ACCOUNT="$(./app_repo_configs account)"
+    ./app_repo_create.sh $NAME $STATUS $ACCOUNT
+    cp -r $MODEL_PATH ../../$NAME
+    cd ../../$NAME
+    git add .
+    git status
+    git commit -m "[DAA]: Criação inicial da aplicação $NAME"
+    git push
+    echo "[DAA]: Repositório da aplicação $NAME criado com sucesso!"
+
+# Destino inválido
 else
-    echo "Tipo desconhecido: $TYPE"
-    exit 1
+    echo "[DAA]: Destino de repositório inválido! Repositório para a aplicação $NAME não foi criado com sucesso!"
 fi
-
-# Realizando Atualização do repositório com os padrões esperados
-./app_repo.sh $NAME $STATUS $ACCOUNT
-cp -r $MODEL_PATH ../../$NAME
-cd ../../$NAME
-git add .
-git status
-git commit -m "[DAA]: Criação inicial da aplicação $NAME"
-git push
-echo "[DAA]: Repositório da aplicação $NAME criado com sucesso!"
---
+++ scripts/test.sh
@@ -0,0 +1,2 @@
+RESULTADO=$(./app_get_types.sh frontend-react)
+echo ">>> $RESULTADO <<<"
